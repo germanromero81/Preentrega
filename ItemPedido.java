33e0a1aad7d3a54ced7ed7dac59dffcd1a913670
germanromero81 <70709063+germanromero81@users.noreply.github.com>
2025-05-25 18:30:05 -0300

    Add files via upload

---
+++ ItemPedido.java
@@ -0,0 +1,23 @@
+package modelo;
+
+public class ItemPedido {
+    private Producto producto;
+    private int cantidad;
+
+    public ItemPedido(Producto producto, int cantidad) {
+        this.producto = producto;
+        this.cantidad = cantidad;
+    }
+
+    public Producto getProducto() {
+        return producto;
+    }
+
+    public int getCantidad() {
+        return cantidad;
+    }
+
+    public double getSubtotal() {
+        return producto.getPrecio() * cantidad;
+    }
+}
